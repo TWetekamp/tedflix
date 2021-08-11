c434c6295a9a3a04f8890d63c51372ba815e2a3c
Ted Wetekamp <ted.wetekamp@yahoo.com>
2021-08-10 19:28:59 -0500

    FileTree and Sticky footer

---
+++ react-ui/src/app.jsx
@@ -0,0 +1,15 @@
+import Header from './sections/header/header';
+import Main from './sections/main/main';
+import Footer from './sections/footer/footer';
+import './globalstyle.css'
+function App() {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
